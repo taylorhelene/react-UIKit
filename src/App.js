53609ff3d7dd51693d81efc7716d5fa9143f3243
taylorhelene <33156281+taylorhelene@users.noreply.github.com>
2023-04-27 15:27:37 +0300

    2 files modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
       </ImageCupContainer>
       <ImageCupContainer  className="extensionimage">
       <ContainerRow className="logo-row">
-        <Image className="" src="https://cdn.jsdelivr.net/gh/taylorhelene/react-UIKit@main/src/Images/pagelogo.png" alt="logo"/>
+        <Image className="" src="https://cdn.jsdelivr.net/gh/taylorhelene/react-UIKit@main/src/Images/logo.png" alt="logo"/>
         <Text className="logo-header">Sanaa__Vijanaa</Text>
       </ContainerRow>
       <ContainerRow className="header-row">
@@ -32,6 +32,7 @@ export default function App() {
       <Button className="button" disabled={false} type="button">Get Started</Button>
       </ImageCupContainer>
       </ContainerRow>
+      
       <Link href="https://react-tutorial.app">This</Link>
      
       <ContainerRow className="containerRow">
